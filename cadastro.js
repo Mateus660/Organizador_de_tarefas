78f4655c4df782468306383400ce40951f8e272b
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2024-12-12 23:19:45 -0300

    alterei o projeto

--- cadastro.js
+++ cadastro.js
@@ -1,17 +1,49 @@
 $(document).ready(function () {
     let nome = $('#nome')
-    let labelnome = $('#labelnome')
+    let labelnome = $('#labelnome p')
+    
+    let usuario = $('#usuario')
+    let labelusuario = $('#labelusuario p')
+  
+    let senha = $('#senha')
+    let labelsenha = $('#labelsenha p')
 
-  $('#cadastrar').click(function () { 
-    alert('ola')
-  });
+    let confirmsenha = $('#confirmsenha')
+    let labelconfirmsenha = $('#labelconfirmsenha p') 
 
-    nome.keyup(()=> { 
-     
+  $(nome).keyup(function () { 
+       
     if (nome.val().trim().length <= 3) {
       labelnome.css({'display':'block'});
     } else {
-        labelnome.css({'display':'none'});      
+      labelnome.css({'display':'none'});      
      }
-  });
+    });
+
+    $(usuario).keyup(function () { 
+       
+      if (usuario.val().trim().length <= 3) {
+        labelusuario.css({'display':'block'});
+      } else {
+        labelusuario.css({'display':'none'});      
+       }
+    });
+
+      $(senha).keyup(function () { 
+       
+        if (senha.val().trim().length <= 3) {
+          labelsenha.css({'display':'block'});
+        } else {
+          labelsenha.css({'display':'none'});      
+         }
+    });
+
+        $(confirmsenha).keyup(function () { 
+       
+          if (confirmsenha.val().trim().length <= 3) {
+            labelconfirmsenha.css({'display':'block'});
+          } else {
+            labelconfirmsenha.css({'display':'none'});      
+           }      
+    });
 });
